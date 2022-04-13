0a15bf9e3e2b81ccedca6a8ff842613cc51da801
JayIssuree <jay_issuree@hotmail.co.uk>
2022-04-13 13:48:41 +0100

    completed day 12

---
+++ Day12/GuessTheNumber.py
@@ -0,0 +1,27 @@
+from random import randrange
+
+print("Welcome to the Number Guessing Game")
+print("I'm thinking of a number between 1 and 100")
+
+NUMBER = randrange(100)
+
+difficulty = raw_input("Choose a difficulty. Type 'easy' or 'hard': ")
+if difficulty == "easy":
+    number_of_guesses = 10
+else:
+    number_of_guesses = 5
+
+while number_of_guesses > 0:
+    print("You have " + str(number_of_guesses) + " attmpts remaining to guess the number.")
+    guess = int(raw_input("Make a guess: "))
+    if guess > NUMBER:
+        print("Too high")
+    elif guess < NUMBER:
+        print("Too low")
+    else:
+        print("Congratulations, you guessed correctly")
+        break
+    print("Guess again")
+    number_of_guesses -= 1
+
+print("The number I was thinking of was: " + str(NUMBER))
